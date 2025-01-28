b6e19adcd8b42ddf82a8086f9eba15d851c19b54
kanjitta <jittakan2539@gmail.com>
2025-01-28 23:18:23 +0700

    Main show start of program and food price

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
+type foodSet struct {
+	Name	string;
+	Price	int;
+}
+
 func main() {
-	
+	fmt.Println("Welcome! This is the Food Store Calculator at your service.")
+	fmt.Println("Here are the list of our scrumptious menu.")
+	fmt.Println("----------------------------------------")
+
+	menu := []foodSet{
+		{"Red set", 50},
+		{"Green set", 40},
+		{"Blue set", 30},
+		{"Yellow set", 50},
+		{"Pink set", 80},
+		{"Purple set", 90},
+		{"Orange set", 120},
+	}
+
+	for i, food := range menu {
+		fmt.Printf("%d. %-15s  %-3d THB/set\n", i+1, food.Name, food.Price)
+	}
+
+
+	// fmt.Println("Please type the number you would like order for each set. If you don't, please type 0.")
 }
