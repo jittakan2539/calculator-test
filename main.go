d14164b269766b5c605368a8582866fa0c810780
kanjitta <jittakan2539@gmail.com>
2025-01-29 09:09:36 +0700

    create input receptor for quantity

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type foodSet struct {
 	Name	string;
 	Price	int;
+}
+
+var reader = bufio.NewReader(os.Stdin)
+
+func getInteger(prompt string) int {
+	fmt.Printf("%v", prompt)
+	input, _ := reader.ReadString('\n')
+	value, err := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+	if err != nil {
+		message, _ := fmt.Scanf("%v must be an integer only.", prompt)
+		panic(message) 
+	}
+
+	return int(value)
 }
 
 func main() {
@@ -25,6 +45,17 @@ func main() {
 	for i, food := range menu {
 		fmt.Printf("%d. %-15s  %-3d THB/set\n", i+1, food.Name, food.Price)
 	}
+
+	fmt.Println("----------------------------------------")
+
+	quantity := make([]int, 7)
+	fmt.Println("Please type how many sets you wpuld like to order. (Type 0 if you don't want that set. )")
+
+	for i, food := range menu {
+		quantity[i] = getInteger(fmt.Sprintf("%s: ", food.Name))
+	}
+
+
 
 
 	// fmt.Println("Please type the number you would like order for each set. If you don't, please type 0.")
